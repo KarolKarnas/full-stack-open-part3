8e7971f9ad0b7c9567f6abbb485c934476f91af9
KarolKarnas <karol.karnas@gmail.com>
2023-05-25 14:38:17 +0200

    Solution for 3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
 	{
 		id: 1,
@@ -52,6 +54,20 @@ app.delete('/api/persons/:id/', (request, response) => {
 	persons = persons.filter((person) => person.id !== id);
 
 	response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+	const id = Number((Math.random() * 10 ** 10).toFixed());
+
+	const person = {
+		id,
+		name: body.name,
+		number: body.number,
+	};
+
+	persons = [...persons, person];
+	response.json(person);
 });
 
 const PORT = 3001;
