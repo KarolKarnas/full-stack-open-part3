cb58bdc3193375018455bde4d0e0fb608aad670c
KarolKarnas <karol.karnas@gmail.com>
2023-06-01 12:09:28 +0200

    Solution for 3.20*: Phonebook database, step8

--- models/person.js
+++ models/person.js
@@ -19,7 +19,16 @@ const personSchema = new mongoose.Schema({
 		minLength: 3,
 		required: true,
 	},
-	number: String,
+	number: {
+		type: String,
+		minLength: 8,
+		validate: {
+			validator: function (v) {
+				return /^\d{2,3}-\d+$/.test(v);
+			},
+			message: "Please provide a valid phone number in the format 'XX-XXXXXX' or 'XXX-XXXXX'.",
+		},
+	},
 });
 
 personSchema.set('toJSON', {
