4758d4fa8719da54d0def5ee6dcda74db0e919bd
KarolKarnas <karol.karnas@gmail.com>
2023-06-01 10:41:37 +0200

    Solution for 3.19*: Phonebook database, step7

--- index.js
+++ index.js
@@ -15,10 +15,14 @@ const unknownEndpoint = (request, response) => {
 
 const errorHandler = (error, request, response, next) => {
 	console.error(error.message);
+	console.error('NAME OF ERROR: ', error.name);
 
 	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id' });
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).send({ error: error.message });
 	}
+
 	next(error);
 };
 
@@ -62,18 +66,18 @@ app.get('/api/persons/:id/', (request, response, next) => {
 });
 
 app.put('/api/persons/:id/', (request, response, next) => {
-	const body = request.body
-	const person = {
-		name: body.name,
-		number: body.number,
-	}
-	Person.findByIdAndUpdate(request.params.id, person, {new: true})
+	const {name, number} = request.body;
+
+	Person.findByIdAndUpdate(request.params.id, {name, number}, {
+		new: true,
+		runValidators: true,
+		context: 'query',
+	})
 		.then((updatedPerson) => {
-			response.json(updatedPerson)
+			response.json(updatedPerson);
 		})
 		.catch((error) => next(error));
 });
-
 
 app.delete('/api/persons/:id/', (request, response, next) => {
 	Person.findByIdAndRemove(request.params.id)
@@ -89,27 +93,30 @@ app.delete('/api/persons/:id/', (request, response, next) => {
 		.catch((error) => next(error));
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
 	const body = request.body;
 
-	if (!body.name) {
-		return response.status(400).json({
-			error: 'name is missing',
-		});
-	} else if (!body.number) {
-		return response.status(400).json({
-			error: 'number is missing',
-		});
-	}
+	// if (!body.name) {
+	// 	return response.status(400).json({
+	// 		error: 'name is missing',
+	// 	});
+	// } else if (!body.number) {
+	// 	return response.status(400).json({
+	// 		error: 'number is missing',
+	// 	});
+	// }
 
 	const person = new Person({
 		name: body.name,
 		number: body.number,
 	});
 
-	person.save().then((savedPerson) => {
-		response.json(savedPerson);
-	});
+	person
+		.save()
+		.then((savedPerson) => {
+			response.json(savedPerson);
+		})
+		.catch((error) => next(error));
 });
 
 app.use(unknownEndpoint);
